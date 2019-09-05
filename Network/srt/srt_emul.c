4bb6de82edbbdf713bd562a54975fb6f2620168f
Swon Lee <andrew.lee@dreamtobe.net>
2019-09-05 16:39:55 +0900

    fix several error

--- Network/srt/srt_emul.c
+++ Network/srt/srt_emul.c
@@ -15,12 +15,13 @@
 char* get_datestr(char* strDate);
 char* get_timestr(char* strTime);
 void logPrn(const char *format, ... );
+int readData(char* filename, char* buffer);
 SRTSOCKET connectSRT(char *src_addr, int src_port, char *dest_addr, int dest_port, bool isRendezvous, bool isServer);
 int createEPoll(SRTSOCKET sock);
 
 int main(int argc , char *argv[]) 
 {
-    char *src_addr = NULL, *dest_addr, *filename, *buffer;
+    char *src_addr = NULL, *dest_addr, *filename, *pbuffer, buffer[BUFFER_SIZE];
     int src_port, dest_port, nRet = 0, pos = 0, snd_size = 1316;
     SRTSOCKET sock, rfd;
     int rfdlen = 1, cnt = 0, totalSize = 0, argcPos = 1;
@@ -53,18 +54,45 @@ int main(int argc , char *argv[])
     isRendezvous = (atoi(argv[argcPos + 4]) == 1)?true:false;
     isServer = (atoi(argv[argcPos = 5]) == 1)?true:false;
 
+    if (!isServer) {
+        nRet = readData(filename, buffer);
+        if (nRet == -1) {
+            return -1;
+        }
+    }
+
     sock = connectSRT(src_addr, src_port, dest_addr, dest_port, isRendezvous, isServer);
     if (sock == SRT_ERROR) {
         return -1;
     }
     logPrn("Sueccess : connect");
 
-
     int pollid = createEPoll(sock);
     if (pollid == SRT_ERROR) {
         srt_close(sock);
         srt_cleanup();
         return -1;
+    }
+
+    if (!isServer) {
+        /// send the size
+        srt_sendmsg(sock, (char *)&nRet , sizeof(nRet) , -1, 1);
+        printf("Send size : %d\n", nRet);
+
+        /// send the data
+#if 0
+        while(nRet > 0) {
+            if (nRet < 1316)
+                snd_size = nRet;
+            srt_sendmsg(sock , buffer + pos , snd_size , -1 , 1);
+            logPrn("send message (%d)" , snd_size);
+            nRet -= snd_size;
+            pos += snd_size;
+        }
+#else
+        srt_sendmsg(sock , buffer, nRet, -1 , 1);
+#endif
+
     }
 
     logPrn("Receiving size : %d\n" , totalSize);
@@ -86,8 +114,8 @@ int main(int argc , char *argv[])
 
     logPrn("Receive size : %d\n" , totalSize);
 
-    buffer = (char *)malloc(totalSize);
-    memset(buffer, 0x00, totalSize);
+    pbuffer = (char *)malloc(totalSize);
+    memset(pbuffer, 0x00, totalSize);
 
     /// receive the data
     while((totalSize > 0) && (cnt < 50)) {
@@ -105,7 +133,7 @@ int main(int argc , char *argv[])
             srt_epoll_release(pollid);
             srt_close(sock);
             srt_cleanup(); 
-            free(buffer);
+            free(pbuffer);
             return -1;
         }
 
@@ -119,31 +147,33 @@ int main(int argc , char *argv[])
     totalSize = pos;
     pos = 0;
 
-    srt_sendmsg(sock, (char *)&totalSize , sizeof(totalSize) , -1, 1);
-    printf("Send size : %d\n", totalSize);
+    if (isServer) {
+        srt_sendmsg(sock, (char *)&totalSize , sizeof(totalSize) , -1, 1);
+        printf("Send size : %d\n", totalSize);
 
-    nRet = totalSize;
+        nRet = totalSize;
 
-    /// send the data
+        /// send the data
 #if 0
-    while(nRet > 0) {
-        if (nRet < 1316)
-            snd_size = nRet;
-        srt_sendmsg(sock , buffer + pos , snd_size , -1 , 1);
-        logPrn("send message (%d)" , snd_size);
-        nRet -= snd_size;
-        pos += snd_size;
-    }
+        while(nRet > 0) {
+            if (nRet < 1316)
+                snd_size = nRet;
+            srt_sendmsg(sock , buffer + pos , snd_size , -1 , 1);
+            logPrn("send message (%d)" , snd_size);
+            nRet -= snd_size;
+            pos += snd_size;
+        }
 #else
-    srt_sendmsg(sock , buffer, totalSize, -1, 1);
+        srt_sendmsg(sock , buffer, totalSize, -1, 1);
 #endif
 
-    if (srt_epoll_wait(pollid, &rfd , &rfdlen, 0, 0, 10 * 1000, 0, 0, 0, 0) < 0) {
-        printf("srt_epoll_wiat timeout (%d)\n" , cnt);
-        cnt++;
+        if (srt_epoll_wait(pollid, &rfd , &rfdlen, 0, 0, 10 * 1000, 0, 0, 0, 0) < 0) {
+            printf("srt_epoll_wiat timeout (%d)\n" , cnt);
+            cnt++;
+        }
     }
 
-    free(buffer);
+    free(pbuffer);
     srt_epoll_remove_usock(pollid, sock);
     srt_epoll_release(pollid);
 
@@ -301,6 +331,29 @@ SRTSOCKET connectSRT(char *src_addr, int src_port, char *dest_addr, int dest_por
 
 }
 
+int readData(char* filename, char* buffer)
+{
+    int nRet = 0;
+    int fd = open(filename , O_RDONLY , 0);
+
+    if (fd < 0) {
+        printf("can't open %s\n", filename);
+        return -1;
+    }
+
+    nRet = read(fd, buffer, BUFFER_SIZE);
+
+    if (nRet <= 0) {
+        printf("can't read file(%s)\n" , filename);
+        close(fd);
+        return -1;
+    }
+
+    close(fd);
+
+    return nRet;
+}
+
 int createEPoll(SRTSOCKET sock)
 {
     /// create srt_epoll
@@ -323,4 +376,8 @@ int createEPoll(SRTSOCKET sock)
     }
 
     return pollid;
+}
+
+int clientAction(SRTSOCKET sock)
+{
 }
